16cbaaf1e9305d5d732dc56fb8912ba77d4354d5
NightmareVCO <vladimircuriel@outlook.com>
2025-09-08 02:40:40 -0400

    feat: add various icon components including Arrow, Call, Facebook, Instagram, LinkedIn, Mail, Phone, Twitter, and Web

---
+++ src/components/icons/arrow-pri.tsx
@@ -0,0 +1,22 @@
+const ArrowPri = () => {
+  return (
+    <svg
+      className="transition-all duration-300 hover:scale-110 fill-primary-200 hover:fill-primary-400"
+      width="19"
+      height="17"
+      viewBox="0 0 19 17"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <title>Arrow Primary</title>
+      <path
+        d="M2 8.43536L15.7232 8.29851M10.6182 1.91132L17.1412 8.4343L10.4868 15.0887"
+        stroke="#292F36"
+        strokeWidth="2"
+        strokeLinecap="square"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
+
+export default ArrowPri
---
+++ src/components/icons/facebook.tsx
@@ -0,0 +1,19 @@
+const Facebook = () => {
+  return (
+    <svg
+      className="transition-all duration-300 hover:scale-110 fill-primary-100 hover:fill-primary-400"
+      width="15"
+      height="25"
+      viewBox="0 0 11 19"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <title>Facebook Icon</title>
+      <path
+        d="M9.68359 10.875L10.1758 7.64062H7.04688V5.53125C7.04688 4.61719 7.46875 3.77344 8.875 3.77344H10.3164V0.996094C10.3164 0.996094 9.01562 0.75 7.78516 0.75C5.21875 0.75 3.53125 2.33203 3.53125 5.14453V7.64062H0.648438V10.875H3.53125V18.75H7.04688V10.875H9.68359Z"
+        fill="primary-100"
+      />
+    </svg>
+  )
+}
+
+export default Facebook
